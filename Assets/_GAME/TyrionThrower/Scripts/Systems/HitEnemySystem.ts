1e9b589d5573df1fef0745ac6e83d3e3eaab76f6
Leandro <leandrotamada@hotmail.com>
2019-02-02 20:11:38 -0200

    Hit system

--- Assets/_GAME/TyrionThrower/Scripts/Systems/HitEnemySystem.ts
+++ Assets/_GAME/TyrionThrower/Scripts/Systems/HitEnemySystem.ts
@@ -7,22 +7,52 @@ namespace game {
     @ut.executeBefore(HitGroundSystem)
     export class HitEnemySystem extends ut.ComponentSystem {
 
-        OnUpdate(): void {
+        //private hero: game.BoxCollider;
+        
+        //private heroOffsetedCollider: game.BoxCollider = new game.BoxCollider();
+        //private enemyOffsetedCollider: game.BoxCollider = new game.BoxCollider();
 
+        OnUpdate(): void
+        {
             if (GameService.IsPaused(this.world))
                 return;
 
-            let config = GameService.GetConfig(this.world);
+            let hero = GameService.GetHeroEntity(this.world);
+            let heroTransform = this.world.getComponentData(hero, ut.Core2D.TransformLocalPosition);
+            let heroCollider = this.world.getComponentData(hero, game.BoxCollider);
+
+            //console.log("hero h: " + heroCollider.height + ", w: " + heroCollider.width);
+            heroCollider.x = heroTransform.position.x - heroCollider.width;
+            heroCollider.y = heroTransform.position.y - heroCollider.height;
+            
             this.world.forEach(
-                [ut.Entity, Hero, Flying, ut.Core2D.TransformLocalPosition],
-                (entity, hero, flying, transform) => {
+                [ut.Entity, game.Enemy, ut.Core2D.TransformLocalPosition, game.BoxCollider],
+                (entity, enemy, transform, collider) =>
+                {
+                    //console.log("enemy w: " + collider.width + ", h: " + collider.height);
+                    let enemyCollider = new BoxCollider();
 
-                    let pos = transform.position;
+                    enemyCollider.x = transform.position.x - collider.width;
+                    enemyCollider.y = transform.position.y - collider.height;
 
-                    //look for enemy. Hit. Fix position
+                    enemyCollider.height = collider.height;
+                    enemyCollider.width = collider.width;
 
-                    transform.position = pos;
+                    if (this.CheckCollision(heroCollider, enemyCollider))
+                    {
+                        console.log(this.world.getEntityName(entity));
+                    }
                 }
+            );
+        }
+
+        CheckCollision(colliderA:BoxCollider, colliderB: BoxCollider): boolean
+        {
+            return !(
+                ((colliderA.y + 2 * colliderA.height) < (colliderB.y)) ||
+                (colliderA.y > (colliderB.y + 2 * colliderB.height)) ||
+                ((colliderA.x + 2 * colliderA.width) < colliderB.x) ||
+                (colliderA.x > (colliderB.x + 2 * colliderB.width))
             );
         }
     }
